e66974b703e9810a2d987b0347a8dde7657e9f00
thelazyone <giacomo.pantalone@gmail.com>
2023-07-31 23:02:40 +0530

    Susan is sent but no closing of the server.

--- rust_code/mesh-streamer/src/lib.rs
+++ rust_code/mesh-streamer/src/lib.rs
@@ -1,7 +1,15 @@
 use std::fs::OpenOptions;
-use stl_io;
-use tungstenite::{connect, Message};
+
+use tokio::runtime::Runtime;
+use tokio::net::TcpListener;
+use tokio_tungstenite::accept_async;
+use tokio_tungstenite::tungstenite::Message;
+use tokio_tungstenite::tungstenite::protocol::CloseFrame;
+use tokio_tungstenite::tungstenite::protocol::frame::coding::CloseCode;
+use std::borrow::Cow;
 use serde_json::json;
+use std::sync::mpsc;
+use futures::{SinkExt, StreamExt};
 
 pub fn load_test(path: String) -> Result<stl_io::IndexedMesh, std::io::Error> {
     let mut file = OpenOptions::new().read(true).open(path).unwrap();
@@ -24,10 +32,6 @@ impl InputMesh {
 
 pub fn send_mesh(input_mesh : &InputMesh) -> Result<(), std::io::Error> {
 
-    let (mut socket, response) = connect("ws://localhost:8765").map_err(|e| {
-        std::io::Error::new(std::io::ErrorKind::Other, format!("WebSocket connection failed: {}", e))
-    })?;
-
     // Create a JSON object from the vertices and triangles
     let model_data = json!({
         "vertices": input_mesh.vertices,
@@ -37,10 +41,11 @@ pub fn send_mesh(input_mesh : &InputMesh) -> Result<(), std::io::Error> {
     // Convert the JSON object to a string
     let message = serde_json::to_string(&model_data)?;
 
-    // Send the message through the WebSocket connection
-    socket.write_message(Message::Text(message))
-        .map_err(|e| std::io::Error::new(
-            std::io::ErrorKind::Other, format!("Failed to send message: {}", e)))?;
+    // Sending the websocket here
+    let rt = Runtime::new().unwrap();
+    rt.block_on(start_server_ws(message)).map_err(|e| {
+        std::io::Error::new(std::io::ErrorKind::Other, format!("Error sending Message: {}", e))
+    })?;
 
     Ok(())
 }
@@ -70,6 +75,63 @@ pub fn send_mesh_stl(indexed_mesh : &stl_io::IndexedMesh) -> Result<(), std::io:
     send_mesh(&input_mesh)
 }
 
+
+// Websocket functions:
+
+async fn send_mesh_ws(
+    mut websocket: tokio_tungstenite::WebSocketStream<tokio::net::TcpStream>,
+    model_data: String,
+    tx: mpsc::Sender<()>
+) -> Result<(), Box<dyn std::error::Error>> {
+
+    websocket.send(Message::Text(model_data)).await?; // Use send method here
+
+    let mut ack = None;
+    while ack.is_none() {
+        if let Some(Ok(msg)) = websocket.next().await { // Use the next method here
+            if msg.is_text() && msg.to_text().unwrap() == "ACK" {
+                ack = Some(());
+            }
+        }
+    }
+
+    let close_frame = CloseFrame {
+        code: CloseCode::Normal,
+        reason: Cow::Borrowed(""),
+    };
+    websocket.close(Some(close_frame)).await?; // Use close method with a CloseFrame
+
+    tx.send(()).unwrap(); 
+    Ok(())
+}
+
+
+async fn start_server_ws(model_data: String) -> Result<(), Box<dyn std::error::Error>> {
+    let mut stop_server = false;
+
+    // Create a TCP listener
+    let listener = TcpListener::bind("localhost:8765").await?;
+    while !stop_server {
+        // Accept an incoming TCP connection
+        if let Ok((stream, _)) = listener.accept().await {
+            // Upgrade the TCP connection to a WebSocket connection
+            let websocket = accept_async(stream).await?;
+
+            // Create a channel for sending messages
+            let (tx, rx) = mpsc::channel();
+
+            // Send the model data to the client
+            send_mesh_ws(websocket, model_data.clone(), tx).await?;
+
+
+            if rx.recv().is_err() {
+                stop_server = true;
+            }
+        }
+    }
+
+    Ok(())
+}
 
 
 #[cfg(test)]
